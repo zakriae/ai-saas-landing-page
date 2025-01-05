673d407c6626f33feb2caf1ee858c6734817811b
Zakariae EL YOUSFI <76444538+zakriae@users.noreply.github.com>
2025-01-05 14:30:01 +0100

    addinh header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.tsx";
-import Button from "./component/Button.tsx";
+import Header from "./component/Header.tsx";
 
 function App() {
   return (
     <>
-      <h5 className="text-3xl font-bold underline ">hello world</h5>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Button className="mt-10" >Something</Button>
+        <Header />
       </div>
-    
+
       <ButtonGradient />
     </>
   );
